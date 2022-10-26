e1a84faf37aa3cac6be17848a435d3c1600ad257
I-Endi <endi.isuf02@gmail.com>
2022-10-26 20:24:41 +0200

    Comments fixed

--- solve.py
+++ solve.py
@@ -10,13 +10,11 @@
 # Author names and student IDs:
 # Endi Isuf (1542591) 
 # Dea Llazo (1589857)
+# Ilesh Yadav (1540025)
 ##
 
 # Import built-in json library for handling input/output 
 import json
-# REMOVE
-from wolframclient.evaluation import WolframLanguageSession
-from wolframclient.language import wl, wlexpr
 from random import randint
 
 
@@ -225,12 +223,12 @@ def poly_division(poly_f, poly_g, mod):
         # Make the takeaway a list and multiply it with the divisor
         takeaway = ([0] * (deg_r - deg_g)) + [takeaway]
         prod = poly_multiplication(takeaway, poly_g, mod)
-        # Subtract the takeaway from the remainder
+        # Subtract the takeaway from the remainder and update
         r = poly_subtraction(r, prod, mod)
         r = poly_clean(r)
         deg_r = len(r) - 1
 
-    poly_f, poly_g = poly_clean(poly_f), poly_clean(poly_g)
+    # Return result
     return poly_clean(q), poly_clean(r)
 
 
@@ -240,18 +238,26 @@ def poly_EEA(poly_f, poly_g, mod):
     denotes the coefficient field and return a,b,gcd such that a*poly_f + b*poly_g = gcd(poly_f, poly_g).
     The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Assign the proper values to x, v, y, u
     x, v, y, u = [1], [1], [0], [0]
 
+    # Iterate untill 
     while poly_g != [0]:
+        # Divide f by g
         q, r = poly_division(poly_f, poly_g, mod)
+        # Assign g to the new f and the remainder to the new g
         poly_f,poly_g = poly_g, r
+        # Assign x,y to x_1 y_1 and u, v to x, y
         x1, y1 = x, y
         x, y = u, v
+        # u = x1 - q*u, v = y1 - q*v
         u = poly_subtraction(x1, poly_multiplication(q, u, mod), mod)
         v = poly_subtraction(y1, poly_multiplication(q, v, mod), mod)
 
+    # x = x*inverse(lc(g)), y = y*inverse(lc(g))
     x, y = poly_clean(x), poly_clean(y)
     x, y = poly_multiplication(x, [inverse(poly_f[-1], mod)], mod), poly_multiplication(y, [inverse(poly_f[-1], mod)], mod)
+    # Find gcd and return
     gcd = poly_multiplication(poly_f, [inverse(poly_f[-1], mod)], mod)
     return x, y, gcd
 
@@ -261,17 +267,23 @@ def poly_irr_check(poly_f, mod):
     Check if the given polynomial 'poly_f' is irreducable or not where 'mod' is the prime modulus that
     denotes the coefficient field. The polynomial is represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Assign and check degree
     deg_f = len(poly_f) - 1
     if deg_f < 2:
         return True
     
+    # Create t and polynomial g
     t = 1
     poly_g = [0, - 1] + ([0]* (pow(mod, t) - 2)) + [1]
 
+    # Check gcd between f and g
     while poly_gcd(poly_f, poly_g, mod) == [1]:
+        # if gcd == 1 then increment t by one and update g
         t += 1
         poly_g = [0, - 1] + ([0]* (pow(mod, t) - 2)) + [1]
 
+    # After the iteration if t is equal to 
+    # the degree of f then true else false
     if t == deg_f:
         return True
     else:
@@ -282,10 +294,13 @@ def poly_irr_gen(deg_f, mod):
     Generate an irreducible polynomial given it's degree as 'deg_f' and the prime modulus that denotes the coefficient 
     field as 'mod'. The polynomial is represented as a list of coefficients up to the degree of the polynomial.
     """    
+    # Generate random polynomial
     poly_f = poly_clean(poly_random_gen(deg_f, mod))
+    # While not irreducible generate another one
     while not poly_irr_check(poly_f, mod):
         poly_f = poly_clean(poly_random_gen(deg_f, mod))
 
+    # Return reducible polynomial
     return poly_f
 
 
@@ -296,6 +311,7 @@ def ff_addition(poly_f, poly_g, mod, poly_h):
     Do a finite field addition on the irreducible polynomials 'poly_f' and 'poly_g' where 'mod' and 'poly_h' define 
     the arithmetic field. The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Add the polynomials and divide by the polynomial modulus
     poly_sum = poly_addition(poly_f, poly_g, mod)
     q, r = poly_division(poly_sum, poly_h, mod)
     return r
@@ -305,6 +321,7 @@ def ff_subtraction(poly_f, poly_g, mod, poly_h):
     Do a finite field subtraction on the irreducible polynomials 'poly_f' and 'poly_g' where 'mod' and 'poly_h' define 
     the arithmetic field. The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Subtract the polynomials and divide by h
     poly_diff = poly_subtraction(poly_f, poly_g, mod)
     q, r = poly_division(poly_diff, poly_h, mod)
     return r
@@ -314,6 +331,7 @@ def ff_multiplication(poly_f, poly_g, mod, poly_h):
     Do a finite field multiplication on the irreducible polynomials 'poly_f' and 'poly_g' where 'mod' and 'poly_h' define 
     the arithmetic field. The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Multiply the polynomals and divide by h
     poly_prod = poly_multiplication(poly_f, poly_g, mod)
     q, r = poly_division(poly_prod, poly_h, mod)
     return r
@@ -323,6 +341,7 @@ def ff_division(poly_f, poly_g, mod, poly_h):
     Do a finite field division on the irreducible polynomials 'poly_f' and 'poly_g' where 'mod' and 'poly_h' define 
     the arithmetic field. The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Inverse poylnomial g and multiply it by f
     poly_inv = poly_clean(ff_inverse(poly_g, mod, poly_h))
     result = ff_multiplication(poly_f, poly_inv, mod, poly_h)
     return result
@@ -333,7 +352,9 @@ def ff_inverse(poly_f, mod, poly_h):
     Find the finite field inverse of the given polynomial, 'poly_f', on the irreducible polynomials 'poly_f' and 'poly_g' where 'mod' and 'poly_h' define 
     the arithmetic field. The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Find the gcd
     x, y, gcd = poly_EEA(poly_f, poly_h, mod)
+    # If there exists an inverse then divide x by h and return
     if gcd == [1]:
         q, r = poly_division(x, poly_h, mod)
         return r
@@ -345,9 +366,12 @@ def ff_primitivity_check(poly_f, mod, poly_h):
     Check whether the given polynomial, 'poly_f' is a primitive polynomial or not in the finite field defined by 'mod' and 'poly_h'.
     The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
-    prime_divisors = find_prime_divisors(mod - 1)
+    # Calculate the order and find the prime divisors
     order = pow(mod, len(poly_f) - 1)
+    prime_divisors = find_prime_divisors(order - 1)
+    # For every prime divisor raise f to the power (order - 1/ the prime number)
     for prime in prime_divisors:
+        # If the exponentiation returns 1 then it is not primitive
         if ff_exp(poly_f, (order - 1) / prime, mod, poly_h) == [1]:
             return False
     
@@ -359,21 +383,25 @@ def ff_primitivity_gen(mod, poly_h):
     Given 'mod' and 'poly_f' which define a finite field, generate a primitive element for this field.
     The polynomials are represented as a list of coefficients up to the degree of the polynomial.
     """
+    # Generate a random polynomial of a ceratin degree
     poly_f = poly_clean(poly_random_gen(len(poly_h) - 1, mod))
+    # If not primitive generate another one
     while not ff_primitivity_check(poly_f, mod, poly_h):
         poly_f = poly_clean(poly_random_gen(len(poly_h) - 1, mod))
 
-    print("irr: " + poly_irr_check(poly_f, mod))
+    # Return primitive polynomial
     return poly_f
 
 
 ### Helper Functions ###
 
-
+# Make both lists equal length
 def adjust_degree(poly_f, poly_g):
 
+    # get the degree
     deg_f, deg_g = len(poly_f), len(poly_g)
 
+    # Append 0 to the smaller list till they're equal
     while deg_f != deg_g:
         if deg_f > deg_g:
             poly_g.append(0)
@@ -384,7 +412,8 @@ def adjust_degree(poly_f, poly_g):
 
     return poly_f, poly_g
 
-    
+
+# Remove leading zeros in the polynomial
 def poly_clean(poly_f):
 
     while len(poly_f) > 1:
@@ -396,6 +425,7 @@ def poly_clean(poly_f):
 
     return poly_f
 
+# Reduce the coefficients in the polynomial by mod
 def poly_reduce(poly_f, mod):
     
     for i in range(len(poly_f)):
@@ -403,13 +433,16 @@ def poly_reduce(poly_f, mod):
     
     return poly_f
 
+# Find the degree of 2 polynomials
 def poly_double_deg(poly_f, poly_g):
     
     return len(poly_f) - 1, len(poly_g) - 1
 
+# Calculate an integer inverse
 def inverse(x, mod):
     return pow(x, -1, mod)
 
+# Calculate the gcd of two polynomials without finding x and y
 def poly_gcd(poly_f, poly_g, mod):
 
     while poly_g != [0]:
@@ -419,6 +452,7 @@ def poly_gcd(poly_f, poly_g, mod):
     gcd = poly_multiplication(poly_f, [inverse(poly_f[-1], mod)], mod)
     return gcd
 
+# Generate a random polynomial of degree 'deg_f'
 def poly_random_gen(deg_f, mod):
 
     poly_f = []
@@ -427,6 +461,7 @@ def poly_random_gen(deg_f, mod):
 
     return poly_f
 
+# Find all prime divisors for number 'a'
 def find_prime_divisors(a):
 
     prime = 2
@@ -440,6 +475,7 @@ def find_prime_divisors(a):
 
     return result
 
+# Raise a polynomial to the given exponent in finite field defined by mod and poly_h
 def ff_exp(poly_f, exp, mod, poly_h):
     
     if exp == 0:
@@ -449,10 +485,10 @@ def ff_exp(poly_f, exp, mod, poly_h):
     while exp > 1:
         if exp % 2 == 0:
             result = poly_multiplication(result, result, mod)
-            exp /= 2
+            exp = exp / 2
         else:
             result = poly_multiplication(result, poly_f, mod)
-            exp -= 1
+            exp = exp - 1
         result = poly_division(result, poly_h, mod)[1]
 
     return result
@@ -461,11 +497,12 @@ def ff_exp(poly_f, exp, mod, poly_h):
     
 
 
+########################################## ASSIGNMENT IS ABOVE. BELOW IS FOR TESTING PURPOSES ONLY ################################################
 
-
-## Testing correctness, check special values and write comments + (generator fix) 
+## Write comments while chekcing for special values
 
 ### Correctness Checking ###
+import time
 
 def compare(comp_loc, ans_loc, exc_loc):
     with open(comp_loc, "r") as computed_file:
@@ -486,23 +523,23 @@ def compare(comp_loc, ans_loc, exc_loc):
     else:
         return "Incorrect"
 
-# for i in range(18):
-#     diff = time.perf_counter()
-#     solve_exercise("Realistic/Exercises/exercise" + str(i) + ".json", "Solved/Realistic/answer" + str(i) + ".json" )
-#     diff = time.perf_counter() - diff
-#     print("Realistic " + str(i) + ": " + str(diff))
+for i in range(18):
+    diff = time.perf_counter()
+    solve_exercise("Realistic/Exercises/exercise" + str(i) + ".json", "Solved/Realistic/answer" + str(i) + ".json" )
+    diff = time.perf_counter() - diff
+    print("Realistic " + str(i) + ": " + str(diff))
 
-# for i in range(18):
-#     diff = time.perf_counter()
-#     solve_exercise("Simple/Exercises/exercise" + str(i) + ".json", "Solved/Simple/answer" + str(i) + ".json" )
-#     diff = time.perf_counter() - diff
-#     print("Simple " + str(i) + ": " + str(diff))
+for i in range(18):
+    diff = time.perf_counter()
+    solve_exercise("Simple/Exercises/exercise" + str(i) + ".json", "Solved/Simple/answer" + str(i) + ".json" )
+    diff = time.perf_counter() - diff
+    print("Simple " + str(i) + ": " + str(diff))
 
 
-# for i in range(18):
-#     print("Realistic " + str(i) + ": " + compare("Solved/Realistic/answer" + str(i) + ".json", "Realistic/Answers/answer" + str(i) + ".json", "Realistic/Exercises/exercise" + str(i) + ".json"))
-# for i in range(18):
-#     print("Simple " + str(i) + ": " + compare("Solved/Simple/answer" + str(i) + ".json", "Simple/Answers/answer" + str(i) + ".json", "Simple/Exercises/exercise" + str(i) + ".json"))
+for i in range(18):
+    print("Realistic " + str(i) + ": " + compare("Solved/Realistic/answer" + str(i) + ".json", "Realistic/Answers/answer" + str(i) + ".json", "Realistic/Exercises/exercise" + str(i) + ".json"))
+for i in range(18):
+    print("Simple " + str(i) + ": " + compare("Solved/Simple/answer" + str(i) + ".json", "Simple/Answers/answer" + str(i) + ".json", "Simple/Exercises/exercise" + str(i) + ".json"))
 
 
 
@@ -514,6 +551,9 @@ def compare(comp_loc, ans_loc, exc_loc):
 
 # print(str(i) + ": " + compare("Solved/Realistic/answer" + str(i) + ".json", "Realistic/Answers/answer" + str(i) + ".json", "Realistic/Exercises/exercise" + str(i) + ".json"))
 
-print(ff_primitivity_gen(13, [2, 2, 2, 2]))
-session = WolframLanguageSession()
-session.evaluate()
+# print(ff_primitivity_gen(5, [4,
+#         1,
+#         2,
+#         1
+#     ]))
+
